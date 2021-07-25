b469d0958bef326c009d220c1ee9d17936443a72
TxnUkeGrl <txnukegrl@gmail.com>
2021-07-25 14:51:38 -0400

    changed solution file

---
+++ MovieApp/Controllers/MovieController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieAPILab.Models;
+using MovieApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieApp.Controllers
+{
+    public class MovieController : Controller
+    {
+        private readonly MovieDAL _movie = new MovieDAL();
+
+        public async Task<IActionResult> Index()
+        {
+            var movie = await _movie.GetMovies();
+            return View(movie);
+        }
+
+        public IActionResult MovieForm()
+        {
+            return View(new Movie());
+        }
+
+        public async Task<IActionResult> AddMovie(Movie movie)
+        {
+            if(ModelState.IsValid)
+            {
+                await _movie.AddMovie(movie);
+                return RedirectToAction("Index");
+            }
+            return View("AddMovie", movie);
+        }
+
+        public async Task<IActionResult> DeleteMovie(int Id)
+        {
+            await _movie.DeleteMovie(Id);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditMovie(int Id)
+        {
+            var movie = await _movie.GetMovie(Id);
+            return View("MovieForm", movie);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditMovie(int Id, Movie editedMovie)
+        {
+            if(ModelState.IsValid)
+            {
+                await _movie.EditMovie(editedMovie, Id);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
+
+       
+
---
+++ MovieApp/Models/MovieDAL.cs
@@ -0,0 +1,56 @@
+﻿using MovieAPILab.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MovieApp.Models
+{
+    public class MovieDAL
+    {
+        public HttpClient GetHttpClient()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:5001");
+
+            return client;
+        }
+
+        public async Task<List<Movie>> GetMovies()
+        {
+            var client = GetHttpClient();
+
+            var response = await client.GetAsync("/api/Movies");
+            //install-package Microsoft.AspNet.WebAPI.Client
+            var movies = await response.Content.ReadAsAsync<List<Movie>>();
+            return movies;
+        }
+
+        public async Task<Movie> GetMovie(int Id)
+        {
+            var client = GetHttpClient();
+            var response = await client.GetAsync($"/api/Movies/{Id}");
+            var movie = await response.Content.ReadAsAsync<Movie>();
+            return movie;
+        }
+
+        public async Task AddMovie(Movie movie)
+        {
+            var client = GetHttpClient();
+            var response = await client.PostAsJsonAsync("/api/Movies", movie);
+        }
+
+        public async Task DeleteMovie(int Id)
+        {
+            var client = GetHttpClient();
+            var response = await client.DeleteAsync($"/api/Movies/{Id}");
+        }
+
+        public async Task EditMovie(Movie newMovie, int Id)
+        {
+            var client = GetHttpClient();
+            var response = await client.PutAsJsonAsync($"/api/Movies/{Id}", newMovie);
+        }
+    }
+}
